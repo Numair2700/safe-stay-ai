66a31ba798dd09483da0466633701e3f7bc23883
Numair Alsaleh <numair270@gmail.com>
2026-04-03 16:04:38 +0300

    Add placeholder feature test to fix CI

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_application_is_running(): void
+    {
+        $this->assertTrue(true);
+    }
+}
